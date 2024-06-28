70ce1f75a7d2a251863e27531e46de4428c7dca9
Danylo Lubenets <danilarff86@gmail.com>
2024-06-28 20:51:22 +0300

    firmvare v1

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,24 @@
 
 // Define pin number
 int ledPin = 13;
+
+int brakeSignalPin = 2;
+int leftTurnSignalInputPin = 3;
+int rightTurnSignalInputPin = 4;
+int leftTurnSignalOutputPin = 5;
+int rightTurnSignalOutputPin = 6;
+
 // Define timer counter
 volatile uint32_t timer1Counter = 0;
+
+decltype(timer1Counter) lastLeftTurnSignalChangeHL = 0;
+decltype(timer1Counter) lastRightTurnSignalChangeHL = 0;
+decltype(timer1Counter) lastBrakeSignalChangeHL = 0;
+uint8_t lastLeftTurnSignalState = LOW;
+uint8_t lastRightTurnSignalState = LOW;
+uint8_t lastBrakeSignalState = LOW;
+
+void updateLastStates();
 
 class InterruptLock {
 public:
@@ -50,8 +66,66 @@ void loop() {
   // Example: Toggle an LED every second
   static decltype(timer1Counter) lastToggle = 0;
   auto timer1CounterVal = getTimer1Counter();
-  if (timer1CounterVal - lastToggle >= 100) {      // 100 * 10ms = 1000ms
-    digitalWrite(ledPin, digitalRead(ledPin) ^ 1); // Toggle LED on pin 13
+  if (timer1CounterVal - lastToggle >= 100) { // 100 * 10ms = 1000ms
+    // digitalWrite(ledPin, digitalRead(ledPin) ^ 1); // Toggle LED on pin 13
     lastToggle = timer1CounterVal;
   }
+
+  // Turn signal logic
+  // const auto leftTurnSignalState = digitalRead(leftTurnSignalInputPin);
+  // const auto rightTurnSignalState = digitalRead(rightTurnSignalInputPin);
+  const auto brakeSignalState = digitalRead(brakeSignalPin);
+
+  auto allowLeftTurnSignal =
+      brakeSignalState == LOW || // Brake signal is not active
+      ((timer1CounterVal - lastLeftTurnSignalChangeHL) <
+           80 && // Turn signal
+                 // change was
+                 // less than 400ms
+       abs(lastLeftTurnSignalChangeHL - lastBrakeSignalChangeHL) >
+           5); // Brake
+               // signal
+               // change
+               // diff more
+               // than 50ms
+
+  auto allowRightTurnSignal =
+      brakeSignalState == LOW ||
+      ((timer1CounterVal - lastRightTurnSignalChangeHL) < 80 &&
+       abs(lastRightTurnSignalChangeHL - lastBrakeSignalChangeHL) > 5);
+
+  // allowLeftTurnSignal = true;
+  // allowRightTurnSignal = true;
+  digitalWrite(leftTurnSignalOutputPin, allowLeftTurnSignal ? LOW : HIGH);
+  digitalWrite(rightTurnSignalOutputPin, allowRightTurnSignal ? LOW : HIGH);
+
+  updateLastStates();
 }
+
+void updateLastStates() {
+  const auto timer1CounterVal = getTimer1Counter();
+
+  const auto leftTurnSignalState = digitalRead(leftTurnSignalInputPin);
+  const auto rightTurnSignalState = digitalRead(rightTurnSignalInputPin);
+  const auto brakeSignalState = digitalRead(brakeSignalPin);
+
+  if (leftTurnSignalState == LOW && lastLeftTurnSignalState == HIGH) {
+    lastLeftTurnSignalChangeHL = timer1CounterVal;
+  }
+  if (rightTurnSignalState == LOW && lastRightTurnSignalState == HIGH) {
+    lastRightTurnSignalChangeHL = timer1CounterVal;
+    // digitalWrite(ledPin, digitalRead(ledPin) ^ 1); // Toggle LED on pin 13
+  }
+  if (brakeSignalState == LOW && lastBrakeSignalState == HIGH) {
+    lastBrakeSignalChangeHL = timer1CounterVal;
+  }
+
+  lastLeftTurnSignalState = leftTurnSignalState;
+  lastRightTurnSignalState = rightTurnSignalState;
+  lastBrakeSignalState = brakeSignalState;
+}
+
+// Possible cases:
+// - Turn signal starts withouth brake signal
+// - Turn signal starts withouth brake signal and brake signal starts
+// - Turn signal starts with brake signal
